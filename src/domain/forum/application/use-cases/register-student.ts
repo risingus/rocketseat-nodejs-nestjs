746e3937a606753ee20d76dd8f95522c474d26a8
Gustavo Lima <gustavo.it@outlook.com>
2024-03-22 16:19:35 -0300

    feat

---
+++ src/domain/forum/application/use-cases/register-student.ts
@@ -0,0 +1,48 @@
+import { Either, right, left } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { Student } from '../../enterprise/entities/student'
+import { StudentsRepository } from '../repositories/students-repository'
+import { HashGenerator } from '../cryptography/hash-generator'
+import { StudentAlreadyExistsError } from './errors/student-already-exists-error'
+
+interface RegisterStudentUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+type RegisterStudentUseCaseResponse = Either<
+  StudentAlreadyExistsError,
+  {
+    student: Student
+  }
+>
+@Injectable()
+export class RegisterStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashGenerator: HashGenerator
+  ) { }
+
+  async execute({
+    name,
+    password,
+    email
+  }: RegisterStudentUseCaseRequest): Promise<RegisterStudentUseCaseResponse> {
+    const studentWithSameEmail = await this.studentsRepository.findByEmail(email)
+
+    if (studentWithSameEmail) {
+      return left(new StudentAlreadyExistsError(email))
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password)
+
+    const student = Student.create({
+      email,
+      name,
+      password: hashedPassword
+    })
+
+    return right({ student })
+  }
+}
