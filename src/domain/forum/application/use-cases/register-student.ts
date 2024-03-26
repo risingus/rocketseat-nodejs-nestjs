a3e6c9329588fc236cf5dad6d67e38ec862ad9fd
Gustavo Lima <gustavo.it@outlook.com>
2024-03-26 16:03:38 -0300

    feat

--- src/domain/forum/application/use-cases/register-student.ts
+++ src/domain/forum/application/use-cases/register-student.ts
@@ -43,6 +43,8 @@ export class RegisterStudentUseCase {
       password: hashedPassword
     })
 
+    await this.studentsRepository.create(student)
+
     return right({ student })
   }
 }
--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,5 +1,4 @@
 import { faker } from '@faker-js/faker'
-
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
